e6f891c9bd39cce3d5943a9dd11fac42fd83787c
James <dsayhong@hanmail.net>
2015-09-07 19:05:49 +0900

    09-04_2

--- src/_09_04_Operator.java
+++ src/_09_04_Operator.java
@@ -1,14 +1,10 @@
 /**
  *  @author bit-user
- *
- *
- *
- *
- *  ~10 이 -11이 되는 이유는 
- *  
- *  
+
+ *  ~10 이 -11이 되는 이유는 Shift(bit 단위)
  *  양수는 0000 0000 0에서 하나씩 1을 채워나가며 숫자를 기록
  *  음수는 1111 1111 -1에서 하나씩 1을 지워나가며 숫자를 기록
+ *  java Operator는 연산이 이루어지면 상위 단위로 변경됨 Integer + Integer = Float
  */
 
 public class _09_04_Operator {
