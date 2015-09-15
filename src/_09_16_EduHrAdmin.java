4c0ae3b8f049049d6824b9e3959a8275d5ac4381
James <dsayhong@hanmail.net>
2015-09-15 21:56:49 +0900

    과제

---
+++ src/_09_16_EduHrAdmin.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+/**
+ * @author James
+ *
+ */
+public class _09_16_EduHrAdmin {
+	
+	private Emp[] EmpList = new Emp[10];
+	
+	public String EmpType;
+	public String EmpName;
+	public int Salary;
+	public String Parttimer;
+	public String RegularEmp;
+	public static int InputNo;
+	
+	
+	public static void EmpAdd(){}
+	public static void EmpDel(){}
+	public static void SendSalary(){}
+	// 사원추가
+	
+	// 사원삭제
+	
+	// 월급
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("//*****************************************//");
+		System.out.println("1.추가  2.삭제  3.급여");
+		System.out.println("//*****************************************//");
+		Scanner scan = new Scanner(System.in);
+		
+		while(null != null);
+		switch (InputNo)
+		{
+		case 1:  EmpAdd(); break;
+		case 2:  EmpDel(); break;
+		case 3:  SendSalary(); break;
+		case 0:  break;
+
+		}
+		
+
+		
+
+	}
+
+}
